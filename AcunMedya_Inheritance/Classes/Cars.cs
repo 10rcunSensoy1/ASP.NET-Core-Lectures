dab509c1de3d352b05b779a7b6fbe6f1ffbe88f6
Orçun Şensoy <sensoyorcun010@gmail.com>
2025-02-28 19:13:59 +0300

    Add project files.

---
+++ AcunMedya_Inheritance/Classes/Cars.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcunMedya_Inheritance.Classes
+{
+    public class Cars
+    {
+        public string Mark;
+        public string Gear;
+        public void Feature_print(String Mark,String Gear)
+        {
+            Console.WriteLine(Mark+" --> "+Gear+" Viteslidir.");
+        }
+    }
+}
---
+++ AcunMedya_Inheritance/Functions/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AcunMedya_Inheritance.Classes;
+
+BMW bmw = new BMW();
+Porsche por = new Porsche();
+Mercedes mer = new Mercedes();
+Togg togg = new Togg();
+Audi au = new Audi();
+Toyota to = new Toyota();
+bmw.Mark = "BMW";
+bmw.Gear = "Düz";
+bmw.Feature_print(bmw.Mark,bmw.Gear);
+por.Mark = "Porsche";
+por.Gear = "Otomatik";
+por.Feature_print(por.Mark, por.Gear);
+mer.Mark = "Mercedes";
+mer.Gear = "Düz";
+mer.Feature_print(mer.Mark, mer.Gear);
+togg.Mark = "Togg";
+togg.Gear = "Otomatik";
+togg.Feature_print(togg.Mark, togg.Gear);
+au.Mark = "Audi";
+au.Gear = "Otomatik";
+au.Feature_print(au.Mark, au.Gear);
+to.Mark = "Toyota";
+to.Gear = "Düz";
+to.Feature_print(to.Mark, to.Gear);
+
+
+
+
+
