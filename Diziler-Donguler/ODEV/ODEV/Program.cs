2d1ee3d31db4adb12a0f0130e419a6aeacf540cc
Orçun Şensoy <sensoyorcun010@gmail.com>
2025-03-22 22:40:32 +0300

    Add files via upload

---
+++ Diziler-Donguler/ODEV/ODEV/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] sayilar = new int[5];
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Lütfen {i + 1}. sayıyı girin: ");
+    sayilar[i] = int.Parse(Console.ReadLine());
+}
+
+int toplam = 0;
+foreach (int sayi in sayilar)
+{
+    toplam += sayi;
+}
+
+double ortalama = (double)toplam / sayilar.Length;
+
+Console.WriteLine($"Girdiğiniz sayıların ortalaması: {ortalama}");
