f98b79f9d85526a0082f4e1992535f0fb994c311
Orçun Şensoy <sensoyorcun010@gmail.com>
2025-03-22 23:33:59 +0300

    Add files via upload

---
+++ Erişim-Belirleyiciler-Değişkenler/ODEV2/ODEV2/Kullanici.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODEV2
+{
+    class Kullanici
+    {
+        private string adSoyad; 
+        private int yas;        
+        public string Email { get; set; } 
+        
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string value)
+        {
+            adSoyad = value;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int value)
+        {
+            yas = value;
+        }
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Ad Soyad: {adSoyad}, Yaş: {yas}, Email: {Email}");
+        }
+
+    }
+}
---
+++ Erişim-Belirleyiciler-Değişkenler/ODEV2/ODEV2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ODEV2;
+
+Kullanici user = new Kullanici();
+
+
+user.SetAdSoyad("Orcun Sensoy");
+user.SetYas(22);
+user.Email = "orcunsensoy2@example.com";
+
+
+user.BilgileriGoster();
