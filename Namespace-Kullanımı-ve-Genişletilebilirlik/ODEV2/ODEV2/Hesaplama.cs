d4794c46ca0a05fd50952afcd927570a2e23cb10
Orçun Şensoy <sensoyorcun010@gmail.com>
2025-03-22 23:50:28 +0300

    Add files via upload

---
+++ Namespace-Kullanımı-ve-Genişletilebilirlik/ODEV2/ODEV2/Hesaplama.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematikIslemleri
+{
+    public class Hesaplama
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+    
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+
+    }
+}
---
+++ Namespace-Kullanımı-ve-Genişletilebilirlik/ODEV2/ODEV2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ProgramTest
+{
+    using MatematikIslemleri;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Hesaplama hesaplama = new Hesaplama();
+
+            
+            int toplam = hesaplama.Topla(87, 41);
+            int carpim = hesaplama.Carp(52, 4);
+
+            
+            Console.WriteLine($"Toplama Sonucu: {toplam}");
+            Console.WriteLine($"Çarpma Sonucu: {carpim}");
+        }
+    }
+}
