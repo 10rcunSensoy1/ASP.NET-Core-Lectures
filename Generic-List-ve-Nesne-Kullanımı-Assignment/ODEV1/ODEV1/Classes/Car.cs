fe5331fda6f8103e4bc88f4651b009987f693ab5
Orçun Şensoy <sensoyorcun010@gmail.com>
2025-03-29 16:23:49 +0300

    Add files via upload

---
+++ Generic-List-ve-Nesne-Kullanımı-Assignment/ODEV1/ODEV1/Classes/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODEV1.Classes
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double FuelConsumption { get; set; } // 100 km'de yaktığı yakıt (litre)
+        public double TotalDistance { get; set; } // Toplam mesafe (km)
+
+        public double CalculateTotalFuel()
+        {
+            return (FuelConsumption / 100) * TotalDistance;
+        }
+
+    }
+}
---
+++ Generic-List-ve-Nesne-Kullanımı-Assignment/ODEV1/ODEV1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ODEV1.Classes;
+
+List<Car> cars = new List<Car>
+            {
+                new Car { Brand = "Toyota", Model = "Corolla", FuelConsumption = 6.5, TotalDistance = 5000 },
+                new Car { Brand = "BMW", Model = "X5", FuelConsumption = 9.0, TotalDistance = 5000 },
+                new Car { Brand = "Mercedes", Model = "C200", FuelConsumption = 8.2, TotalDistance = 5000 }
+            };
+foreach (var car in cars)
+{
+    double totalFuel = car.CalculateTotalFuel();
+    Console.WriteLine($"Marka: {car.Brand}, Model: {car.Model}, 100 km'de Yaktığı Yakıt: {car.FuelConsumption} litre, Toplam Yakıt Tüketimi: {totalFuel} litre");
+}
+
+
